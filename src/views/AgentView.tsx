f25944fb28edfc6d87af5375e594b86658f07a91
joshbakergf <josh.baker@go-forth.com>
2025-07-09 15:52:55 -0400

    Update AgentView.tsx

--- src/views/AgentView.tsx
+++ src/views/AgentView.tsx
@@ -1,13 +1,6 @@
 import React, { useState } from 'react';
-import ScriptRunner from '../components/ScriptRunner.tsx';
+import ContactForm from './components/ContactForm.tsx';
 
 export default function AgentView() {
-  const [sessionId, setSessionId] = useState(null);
-
-  return (
-    <div>
-      <h2>Call Script</h2>
-      <ScriptRunner sessionId={sessionId} />
-    </div>
-  );
+  return <ContactForm />;
 }
