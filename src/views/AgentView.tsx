d067e904b5bd45163f47a9c1dc814714a30b3020
joshbakergf <josh.baker@go-forth.com>
2025-07-09 16:00:42 -0400

    Update AgentView.tsx

--- src/views/AgentView.tsx
+++ src/views/AgentView.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ContactForm from '../components/ContactForm';
+import ContactForm from '../components/ContactForm.tsx';
 
 export default function AgentView() {
   return (
